c5bd1bac65b53f8aa636b86d79371fe1694ff2d7
EngineNoir <locomatas@gmail.com>
2022-11-08 17:03:04 +0100

    more on rust

--- rust/ca_trial.rs
+++ rust/ca_trial.rs
@@ -32,4 +32,26 @@ fn main() {
 
       println!("{sum}, or as a function, we get {}", sum_2());
 
+      // Check scopes
+      let numbah = 10;
+      {
+          let numbah = 29;
+          println!("{numbah} same variable in block");
+      }
+      println!("{numbah} same variable outside of block");
+
+      // Make variables public by adding pub
+      mod numbers {
+          pub const ZERO: i32 = 0;
+      }
+      // mod defines a Module which has its own scope.
+      
+      mod another_scope {
+          use super::numbers::ZERO;
+
+          fn print_zero() {
+              println!("{ZERO} called up from another scope by virtue of being a public variable.");
+          }
+
+
 }
